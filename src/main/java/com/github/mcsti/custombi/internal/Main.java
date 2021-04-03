0aac31853a8c32be2879121841bb95996682d2bb
enimaloc <github@enimaloc.ga>
2021-04-03 18:50:20 +0200

    refactor(items): Moved commands initialisation in module class

--- src/main/java/com/github/mcsti/custombi/internal/Main.java
+++ src/main/java/com/github/mcsti/custombi/internal/Main.java
@@ -16,22 +16,6 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         itemsModule = new ItemsModuleImpl(this);
-        getCommand("items").setExecutor((commandSender, command, s, args) -> {
-            if (!(commandSender instanceof Player)) {
-                return false;
-            }
-            for (CustomItem customItem : itemsModule.getItems().stream()
-                                                    .filter(item -> item.getNamespace().toString().equalsIgnoreCase(args[0]))
-                                                    .collect(Collectors.toList())) {
-                ((Player) commandSender).getInventory().addItem(customItem.getItemStack(1));
-            }
-            return true;
-        });
-        getCommand("items").setTabCompleter((commandSender, command, s, args) -> itemsModule.getItems()
-                                                                                            .stream()
-                                                                                            .map(CustomItem::getNamespace)
-                                                                                            .map(NamespacedKey::toString)
-                                                                                            .collect(Collectors.toList()));
     }
 
     public ItemsModule getItemsModule() {
--- src/main/java/com/github/mcsti/custombi/internal/items/ItemsModuleImpl.java
+++ src/main/java/com/github/mcsti/custombi/internal/items/ItemsModuleImpl.java
@@ -3,8 +3,13 @@ package com.github.mcsti.custombi.internal.items;
 import com.github.mcsti.custombi.internal.Main;
 import com.github.mcsti.custombi.api.items.ItemsModule;
 import org.bukkit.NamespacedKey;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ItemsModuleImpl implements ItemsModule {
     
@@ -17,6 +22,7 @@ public class ItemsModuleImpl implements ItemsModule {
         
         this.listener = new ItemListener(this);
         plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+        setupCommand(plugin);
     }
     
     @Override
@@ -35,5 +41,28 @@ public class ItemsModuleImpl implements ItemsModule {
                 .stream()
                 .filter(item -> item.getNamespace().equals(namespace))
                 .findFirst();
+    }
+    
+    private void setupCommand(JavaPlugin plugin) {
+        PluginCommand command = plugin.getCommand("items");
+        if (command == null) {
+            return;
+        }
+        command.setExecutor((commandSender, cmd, s, args) -> {
+            if (!(commandSender instanceof Player)) {
+                return false;
+            }
+            for (CustomItem customItem : getItems().stream()
+                                                   .filter(item -> item.getNamespace().toString().equalsIgnoreCase(args[0]))
+                                                   .collect(Collectors.toList())) {
+                ((Player) commandSender).getInventory().addItem(customItem.getItemStack(1));
+            }
+            return true;
+        });
+        command.setTabCompleter((commandSender, cmd, s, args) -> getItems()
+                .stream()
+                .map(CustomItem::getNamespace)
+                .map(NamespacedKey::toString)
+                .collect(Collectors.toList()));
     }
 }
